5b4e924407373a7f2ff67f1d20c293805ff5d505
Koray Liman <koraylimancre@gmail.com>
2024-12-29 22:15:45 +0300

    export dio package

--- lib/flcore.dart
+++ lib/flcore.dart
@@ -1,6 +1,8 @@
 /// A Core Library for Flutter Applications
 library;
 
+export 'package:dio/dio.dart';
+
 export 'src/common/common.dart';
 export 'src/core/core.dart';
 export 'src/utils/utils.dart';
